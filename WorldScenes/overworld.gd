80f881dd7c63a4c22e5cfa3af1ef3e26d2966829
anareyes16 <anamereyes04@unitec.edu>
2023-11-05 17:19:51 -0600

    cueva

---
+++ WorldScenes/overworld.gd
@@ -0,0 +1,19 @@
+extends Node2D
+var current_scene = "overworld"
+var change_scene = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _on_cave_body_entered(body):
+	if body.is_in_group("Player"):
+		change_scene = true
+		change_scenes()
+
+func change_scenes():
+	if change_scene == true:
+		if current_scene == "overworld":
+			get_tree().change_scene_to_file("res://cave.tscn")
---
+++ cave.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+var change_scene = false
+var current_scene = "cave"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func change_scenes():
+	if change_scene == true:
+		if current_scene == "cave":
+			get_tree().change_scene_to_file("res://WorldScenes/overworld.tscn")
+
+
+
+func _on_overworld_body_entered(body):
+	if body.is_in_group("Player"):
+		change_scene == true
+		change_scenes()
